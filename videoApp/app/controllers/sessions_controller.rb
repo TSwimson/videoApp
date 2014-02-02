215c4217cad51336161834c849965aec15e32f30
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-02 11:35:38 -0800

    Show invalid email/password error

--- videoApp/app/controllers/sessions_controller.rb
+++ videoApp/app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
 	def create
     user=User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
+      # Sign the user in and reload root
       sign_in user
       redirect_to '/'
     else
-      # Create an error message and re-render the signin form.
-      flash.now[:error]='Invalid email/password combination'
+      # Create an error message and re-render the page
+      flash[:error]='Invalid email/password combination'
       redirect_to '/'
     end
   end
--- videoApp/app/models/user.rb
+++ videoApp/app/models/user.rb
@@ -6,7 +6,10 @@ class User < ActiveRecord::Base
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true
     validates :password, length: {minimum: 6}
-
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence:   true,
+                   format:     { with: VALID_EMAIL_REGEX },
+                   uniqueness: { case_sensitive: false }
     private
 
     def create_remember_token
