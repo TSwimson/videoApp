4155d54d6b1560e7689b1e20c0b19adef7f9592d
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-06 13:55:18 -0800

    sdf

--- app/controllers/my_heywatch_controller.rb
+++ app/controllers/my_heywatch_controller.rb
@@ -3,11 +3,11 @@ skip_before_filter :verify_authenticity_token
   def upload_complete
     puts "upload_complete params below"
     puts params
-    #binding.pry
+    binding.pry
 
      puts "request body"
-     binding.pry
-     puts request.body
+     
+     puts request.body.read
      render json: {}
      #encode params[:id], params['video_id']
   end
@@ -20,15 +20,15 @@ skip_before_filter :verify_authenticity_token
       :output_url => "s3://#{ENV['AMAZON_ID']}:#{ENV['AMAZON_KEY']}@videosok/converted/abcd.mp4", 
       ping_url: "http://easyvid.heroku.com/hw/encoded/#{id}"
     }
-    #binding.pry
+    binding.pry
   end
 
   def encode_complete
     puts "encode_complete params below"
     puts params
     puts "request body"
-     puts request.body
-    #binding.pry
+     puts request.body.read
+    binding.pry
     video = Video.find(params[:id])
     video.url = params[:output_url]
     video.processed = "processed"
