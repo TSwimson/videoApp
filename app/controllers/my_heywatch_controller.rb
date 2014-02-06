bd467188c2a78ef2ea8913aba6ef82c76a5b1ed1
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-06 15:19:37 -0800

    REmoved bindings

--- app/controllers/my_heywatch_controller.rb
+++ app/controllers/my_heywatch_controller.rb
@@ -3,7 +3,6 @@ skip_before_filter :verify_authenticity_token
   def upload_complete
     puts "upload_complete params below"
     puts params
-    binding.pry
 
      puts "request body"
      
@@ -20,7 +19,6 @@ skip_before_filter :verify_authenticity_token
       :output_url => "s3://#{ENV['AMAZON_ID']}:#{ENV['AMAZON_KEY']}@videosok/converted/abcd.mp4", 
       ping_url: "http://easyvid.heroku.com/hw/encoded/#{id}"
     }
-    binding.pry
   end
 
   def encode_complete
@@ -28,7 +26,6 @@ skip_before_filter :verify_authenticity_token
     puts params
     puts "request body"
     puts request.body.read
-    binding.pry
     video = Video.find(params[:id])
     video.url = params[:output_url]
     video.processed = "processed"
